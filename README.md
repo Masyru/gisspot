c4187a17a1bb0c2a4ca2b16ba74bb715d9acb6c1
Student <user1@iMac-Student4.dvfu.ru>
2021-03-26 16:07:14 +1000

    Edited README and install.sh. Made toolbar with style

--- README.md
+++ README.md
@@ -7,6 +7,7 @@ You need to install:
 + [Nodejs](https://nodejs.org/en/)
 + [Python3.x (upper 3.6) ](https://www.python.org/downloads/release/python-388/)
 + [Anaconda](https://www.anaconda.com/products/individual)
++ [KUDA](https://developer.nvidia.com/cuda-downloads) - if you have <b>nvidia videocard<b/>
 
 ### Installations
 
--- frontend/components/Toolbar.js
+++ frontend/components/Toolbar.js
@@ -1,11 +1,31 @@
 import React, { useState } from "react";
 import "./Toolbar.css";
 
-export const Toolbar = () => {
 
-    let toolbar =
+function Button(props){
+    const butt =
+        <button type={'button'} title={props.title} onClick={props.setType} disabled={props.active} >
+                <img src={`../static/cursors/${props.cursor}.svg`} alt="" width={props.width} height={props.height}/>
+        </button>;
+
+    return butt;
+}
+
+export const Toolbar = () => {
+    // TODO need to indicate typeOfCursor and turn another mode for editting the map
+    const [typeOfCursor, setTypeOfCursor] = useState(0);
+
+    const toolbar =
         <div className={"toolbar"}>
-            Toolbar
+            <Button active={typeOfCursor === 0} title={"Выбрать курсор"} width={"15px"} height={"15px"} cursor={"compass-light"} setType={() => setTypeOfCursor(0)}/>
+            <Button active={typeOfCursor === 1} title={"Выбрать регион"} width={"20px"} height={"20px"} cursor={"selection"} setType={() => {setTypeOfCursor(1)}}/>
+            <Button active={typeOfCursor === 2} title={"Сбросить"} width={"20px"} height={"20px"} cursor={"remove"}
+                setType={() => {
+                        setTypeOfCursor(2);
+                        // TODO function to unzoom to first view on planet
+                    }
+                }
+            />
         </div>;
 
     return(toolbar)
