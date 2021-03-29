342126cfec52faef4c0ffc99be2c87dcd598ff26
Baderik <baderikpavel@gmail.com>
2021-03-29 19:03:22 +1000

    Added base function for worker;

--- backend/worker/services.py
+++ backend/worker/services.py
@@ -1,3 +1,4 @@
+from typing import Optional, Tuple
 from rq import Worker, Queue, Connection
 
 from backend.worker.core import conn
@@ -10,3 +11,33 @@ def create_worker(queues):
     with Connection(conn):
         worker = Worker(map(Queue, queues))
         worker.work(with_scheduler=True)
+
+
+def get_file(url_pro: Optional[str]) -> bytes:
+    pass
+
+
+def worker_processing(url_pro_1: Optional[str],
+                      url_pro_2: Optional[str],
+                      point: Tuple[float, float],
+                      ws_id: Optional[int],
+                      window_size: Tuple[float, float],
+                      vicinity_size: Tuple[float, float]) -> dict:
+    file_1 = get_file(url_pro_1)
+    file_2 = get_file(url_pro_2)
+    b0_file_1, data_file_1 = parse(file_1)
+    b0_file_2, data_file_2 = parse(file_2)
+
+    file_1, file_2 = numpy2torch(data_file_1, data_file_2)
+    try:
+        pos, score, _, _ = find_best_match(file_1, file_2, (point[1], point[0]), window_size, vicinity_size)
+        speed = calculate_distance(b0_file_1, point, pos)
+        return {"x1": point[1], "y1": point[0],
+                "x2": pos[1], "y2": pos[0],
+                "speed": speed,
+                "error_message": None}
+    except AttributeError as e:
+        return {"x1": 0, "y1": 0,
+                "x2": 0, "y2": 0,
+                "speed": 0,
+                "error_message": e}
