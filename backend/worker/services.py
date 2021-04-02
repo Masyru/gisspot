58724135c3a80c4f04d685ec1b08d08ebaeb8d48
Baderik <baderikpavel@gmail.com>
2021-04-02 17:32:28 +1000

    Added new function in worker;

--- backend/worker/services.py
+++ backend/worker/services.py
@@ -5,7 +5,7 @@ from requests import post
 from .core import conn
 from .settings import WORKER_TYPES, QUEUE_URL
 
-from ml.gisalgo import parse, numpy2torch, find_best_match, calculate_distance
+from ...ml.gisalgo import inference, numpy2torch, parse, ssim
 
 
 def create_worker(queues):
@@ -23,27 +23,38 @@ def get_file(url_pro: Optional[str]) -> bytes:
 def worker_processing(b0_file_1,
                       data_file_1,
                       data_file_2,
+                      deltatime: int,
                       point: Tuple[float],
-                      window_size: Tuple[float],
-                      vicinity_size: Tuple[float]) -> dict:
-    file_1, file_2 = numpy2torch(data_file_1, data_file_2)
+                      window_size: Tuple[int],
+                      vicinity_size: Tuple[int]) -> dict:
     try:
-        pos, score, _, _ = find_best_match(file_1, file_2,
-                                           (point[1], point[0]),
-                                           window_size, vicinity_size, "ssim", "ssim")
-        speed = calculate_distance(b0_file_1, point, pos)
-        return {"x1": point[1], "y1": point[0],
-                "x2": pos[1], "y2": pos[0],
-                "speed": speed,
-                "error_message": None}
+        (lat, lon), velocity, mask = inference(data_file_1, data_file_2, b0_file_1, deltatime, point, window_size, vicinity_size, ssim, "cpu", sf=0.5)
+        if mask:
+            res = {"points": [[point[1], point[0]], [lon, lat]],
+                   "velocity": velocity,
+                   "error": None}
+        else:
+            res = {"points": [[0, 0], [0, 0]],
+                   "velocity": 0,
+                   "error": "Что-то про маску"}
     except AttributeError as e:
-        return {"x1": 0, "y1": 0,
-                "x2": 0, "y2": 0,
-                "speed": 0,
-                "error_message": e}
+        if e == "0":
+            res = {"points": [[0, 0], [0, 0]],
+                   "velocity": 0,
+                   "error": "Точка вышла заграницы"}
+        elif e == "1":
+            res = {"points": [[0, 0], [0, 0]],
+                   "velocity": 0,
+                   "error": "Слишком много шума"}
+        else:
+            res = {"points": [[0, 0], [0, 0]],
+                   "velocity": 0,
+                   "error": "Аааааа"}
+    return res
 
 
-def add_worker(task_type: Optional[str] = "low", *params, **kwargs) -> None:
+def add_worker(task_type: Optional[str] = "low",
+               *params, **kwargs) -> None:
     post(QUEUE_URL, json={"params": params, "kwargs": kwargs,
                           "task_type": task_type})
 
@@ -51,11 +62,13 @@ def add_worker(task_type: Optional[str] = "low", *params, **kwargs) -> None:
 def big_worker(url_pro_1: Optional[str],
                url_pro_2: Optional[str],
                points: Tuple[Tuple[float]],
+               deltatime: int,
                window_size: Tuple[int],
                vicinity_size: Tuple[int]) -> None:
     file_1 = get_file(url_pro_1)
     file_2 = get_file(url_pro_2)
     b0_file_1, data_file_1 = parse(file_1)
     b0_file_2, data_file_2 = parse(file_2)
+    data_file_1, data_file_2 = numpy2torch(data_file_1, data_file_2)
     for point in points:
-        add_worker(b0_file_1, data_file_1, data_file_2, point, window_size, vicinity_size)
+        add_worker(b0_file_1, data_file_1, data_file_2, deltatime, point, window_size, vicinity_size)
